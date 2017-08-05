b7eeb35318915e9eb22a467eca0dbbeafa3b417d
Rijal Asepnugroho <rijal.asep.nugroho@gmail.com>
2017-08-05 13:04:41 +0700

    [02-config-file] read config from config.json using viper

---
+++ 02-config-file/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"time"
+	"net/http"
+	"encoding/json"
+	"strconv"
+	"github.com/spf13/viper"
+	"fmt"
+)
+
+type User struct {
+	ID	        int64
+	Name        string
+	Email       string
+	CreatedAt   time.Time
+	UpdateAt    time.Time
+	DeletedAt   time.Time
+}
+
+var users []User
+
+func main() {
+    viper.SetConfigFile("config.json")
+    err := viper.ReadInConfig()
+    if err != nil {
+        fmt.Println(err.Error())
+        panic(err.Error())
+    }
+    
+    router := mux.NewRouter()
+    users = append(users, User{ ID: 1, Name: "Jacky Chan"})
+    users = append(users, User{ ID: 2, Name: "Jet Lee", Email: "jet@lee.com"})
+    
+    router.HandleFunc("/api/users", GetUsersEndPoint).Methods("GET")
+    router.HandleFunc("/api/users", CreateUserEndPoint).Methods("POST")
+    router.HandleFunc("/api/users/{id}", GetUserEndPoint).Methods("GET")
+    
+    http.ListenAndServe(viper.GetString("server.address"), router)
+}
+
+func GetUsersEndPoint(w http.ResponseWriter, _ *http.Request) {
+    json.NewEncoder(w).Encode(users)
+}
+
+func CreateUserEndPoint(w http.ResponseWriter, req *http.Request) {
+    var user User
+    
+    user.Name   = req.FormValue("name")
+    user.Email  = req.FormValue("email")
+    
+    // Validation
+    if len(user.Name) == 0 {
+        json.NewEncoder(w).Encode("Please suplay valid name")
+        return
+    }
+    
+    if len(user.Email) == 0 {
+        json.NewEncoder(w).Encode("Please suplay valid email")
+        return
+    }
+     
+    user.ID         = int64(len(users)+1)
+    user.CreatedAt  = time.Now()
+    
+    users = append(users, user)    
+    json.NewEncoder(w).Encode(user)
+}
+
+func GetUserEndPoint(w http.ResponseWriter, req *http.Request) {
+    params := mux.Vars(req)
+    id, _  := strconv.ParseInt(params["id"], 10, 64)
+    
+    for _, v := range users {
+        if v.ID == id {
+            json.NewEncoder(w).Encode(v)
+            return
+        }
+    }      
+    
+    json.NewEncoder(w).Encode(&User{})
+}
