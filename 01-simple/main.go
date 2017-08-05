4b63940eee480faeb3e25a8d009606826353e2bc
Rijal Asepnugroho <rijal.asep.nugroho@gmail.com>
2017-08-05 12:19:28 +0700

    [01-simple] add create user api

--- 01-simple/main.go
+++ 01-simple/main.go
@@ -24,10 +24,35 @@ func main() {
     users = append(users, User{ ID: 2, Name: "Jet Lee", Email: "jet@lee.com"})
     
     router.HandleFunc("/api/users", GetUserEndPoint).Methods("GET")
+    router.HandleFunc("/api/users", CreateUserEndPoint).Methods("POST")
     
     http.ListenAndServe(":9090", router)
 }
 
 func GetUserEndPoint(w http.ResponseWriter, _ *http.Request) {
     json.NewEncoder(w).Encode(users)
+}
+
+func CreateUserEndPoint(w http.ResponseWriter, req *http.Request) {
+    var user User
+    
+    user.Name   = req.FormValue("name")
+    user.Email  = req.FormValue("email")
+    
+    // Validation
+    if len(user.Name) == 0 {
+        json.NewEncoder(w).Encode("Please suplay valid name")
+        return
+    }
+    
+    if len(user.Email) == 0 {
+        json.NewEncoder(w).Encode("Please suplay valid email")
+        return
+    }
+     
+    user.ID         = int64(len(users)+1)
+    user.CreatedAt  = time.Now()
+    
+    users = append(users, user)    
+    json.NewEncoder(w).Encode(user)
 }
